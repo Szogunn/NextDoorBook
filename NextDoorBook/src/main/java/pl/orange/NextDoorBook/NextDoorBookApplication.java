4d97b70d79476bed37638e3d3640f47862f4560c
szogunn <mateusz1_boczek1@interia.pl>
2023-05-16 19:31:09 +0200

    Spring project initialize

---
+++ NextDoorBook/src/main/java/pl/orange/NextDoorBook/NextDoorBookApplication.java
@@ -0,0 +1,13 @@
+package pl.orange.NextDoorBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextDoorBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextDoorBookApplication.class, args);
+	}
+
+}
---
+++ NextDoorBook/src/test/java/pl/orange/NextDoorBook/NextDoorBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.orange.NextDoorBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextDoorBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
