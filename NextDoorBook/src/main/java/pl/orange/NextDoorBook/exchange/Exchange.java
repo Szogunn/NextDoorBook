b87bb9bbaed0db54c119601200b70fc43e599463
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-17 20:41:28 +0200

    add Exchange entity

---
+++ NextDoorBook/src/main/java/pl/orange/NextDoorBook/exchange/Exchange.java
@@ -0,0 +1,25 @@
+package pl.orange.NextDoorBook.exchange;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import pl.orange.NextDoorBook.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "EXCHANGES")
+public class Exchange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime startRent;
+    private LocalDateTime endRent;
+    @ManyToOne
+    @JoinColumn(name = "OWNER_ID")
+    private User owner;
+    @ManyToOne
+    @JoinColumn(name = "RENTER_ID")
+    private User renter;
+}
