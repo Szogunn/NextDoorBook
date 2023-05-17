d5049707799e4d73833509d912347e4ba1fda5c2
szogunn <mateusz1_boczek1@interia.pl>
2023-05-17 20:43:31 +0200

    [add] Created entities Author, Book, Comment for schema

---
+++ src/main/java/pl/orange/NextDoorBook/author/entity/Author.java
@@ -0,0 +1,29 @@
+package pl.orange.NextDoorBook.author.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import pl.orange.NextDoorBook.book.entity.Book;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "AUTHORS")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "authors_books",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private Set<Book> books = new HashSet<>();
+}
