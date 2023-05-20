ba8f002d0c1fadc81a4a6119bdcb994d8028886e
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-20 11:56:03 +0200

    [add] create class CommentRepository for CRUD method

---
+++ src/main/java/pl/orange/NextDoorBook/comment/CommentRepository.java
@@ -0,0 +1,32 @@
+package pl.orange.NextDoorBook.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import pl.orange.NextDoorBook.book.Book;
+import pl.orange.NextDoorBook.user.User;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepository {
+
+    private final ICommentRepository iCommentRepository;
+
+    public void addComment(Comment comment) {
+        iCommentRepository.save(comment);
+    }
+
+    public void deleteCommentByID(Long id) {
+        iCommentRepository.deleteById(id);
+    }
+
+    public Optional<Comment> getCommentByID(Long id) {
+        return iCommentRepository.findById(id);
+    }
+
+    public void updateComment(Long id, String message, boolean spoilerAlert, Book book, User user) {
+        iCommentRepository.updateComment(id, message, spoilerAlert, book, user);
+    }
+
+}
