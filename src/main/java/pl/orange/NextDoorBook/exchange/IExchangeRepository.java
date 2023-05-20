f84e5d9984b22b0f7ab0bd59b60467c87bc6c972
Bartek <bartlomiejtolak7@gmail.com>
2023-05-20 12:09:51 +0200

    [add]add interface IExchangeRepository with update method implementation

---
+++ src/main/java/pl/orange/NextDoorBook/exchange/IExchangeRepository.java
@@ -0,0 +1,26 @@
+package pl.orange.NextDoorBook.exchange;
+
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.orange.NextDoorBook.book.Book;
+import pl.orange.NextDoorBook.user.User;
+
+import java.time.LocalDate;
+
+
+public interface IExchangeRepository extends JpaRepository<Exchange, Long> {
+
+    @Query(value = "UPDATE Exchange SET startRent = :startRent , endRent =:endRent, owner =:owner, renter =:renter, book =:book WHERE id=:id")
+    @Modifying
+    @Transactional
+    void updateExchange(@Param("id") Long id,
+                        @Param("startRent") LocalDate startRent,
+                        @Param("endRent") LocalDate endRent,
+                        @Param("owner") User owner,
+                        @Param("renter") User renter,
+                        @Param("book") Book book);
+}
