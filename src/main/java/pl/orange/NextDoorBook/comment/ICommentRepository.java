42a01116e755053bd9fc5ba8ee8bafadad9e902e
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-20 11:54:07 +0200

    [add] create interface ICommentRepository extent JPARepository with update query

---
+++ src/main/java/pl/orange/NextDoorBook/comment/ICommentRepository.java
@@ -0,0 +1,25 @@
+package pl.orange.NextDoorBook.comment;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.orange.NextDoorBook.book.Book;
+import pl.orange.NextDoorBook.user.User;
+
+public interface ICommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query(value = "UPDATE Comment SET message = :message" +
+            ",spoilerAlert =:spoilerAlert" +
+            ",book=:book" +
+            ",user=:user" +
+            " WHERE id =:id")
+    @Modifying
+    @Transactional
+    void updateComment(@Param("id") Long id,
+                       @Param("message") String message,
+                       @Param("spoilerAlert") boolean spoilerAlert,
+                       @Param("book") Book book,
+                       @Param("user") User user);
+}
