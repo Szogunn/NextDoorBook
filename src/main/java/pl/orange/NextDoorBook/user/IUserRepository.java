9ae91e0cd06c328ce7657f617fff337aa7af2634
Bartek <bartlomiejtolak7@gmail.com>
2023-05-20 11:21:53 +0200

    [add]add interface IUserRepostiory with update method implementation

---
+++ src/main/java/pl/orange/NextDoorBook/user/IUserRepository.java
@@ -0,0 +1,25 @@
+package pl.orange.NextDoorBook.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import pl.orange.NextDoorBook.address.Address;
+import pl.orange.NextDoorBook.comment.Comment;
+
+import java.util.Set;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+
+
+    @Query(value="UPDATE User SET id=:id,login=:login,password=:password,email=:email,address=:address,comments=:comments WHERE id=:id")
+    @Modifying
+    @Transactional
+    void updateUser(@Param("id")Long id,
+                    @Param("login")String login,
+                    @Param("password")char[] password,
+                    @Param("email")String email,
+                    @Param("address")Address address,
+                    @Param("comments")Set<Comment> comments);
+}
