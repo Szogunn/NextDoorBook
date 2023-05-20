50f4df85edbd9b241800d604bdab14d82dc60b78
szogunn <mateusz1_boczek1@interia.pl>
2023-05-20 11:33:52 +0200

    [add] Delete method implemented in Book Service

--- src/main/java/pl/orange/NextDoorBook/book/BookService.java
+++ src/main/java/pl/orange/NextDoorBook/book/BookService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -13,15 +15,27 @@ public class BookService {
 
     public ResponseEntity<Book> addBook(Book book){
         if (book == null){
-            System.out.println("cos");
             return ResponseEntity
                     .status(404)
                     .build();
-
         }
         bookRepository.addBook(book);
         return ResponseEntity
                 .status(201)
+                .build();
+    }
+
+    public ResponseEntity<Book> deleteBook(Long id){
+        Optional<Book> bookByID = bookRepository.getBookByID(id);
+
+        if (bookByID.isPresent()){
+            bookRepository.deleteBookByID(id);
+            return ResponseEntity
+                    .status(200)
+                    .build();
+        }
+        return ResponseEntity
+                .status(404)
                 .build();
     }
 
