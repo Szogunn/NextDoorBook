789564227f1956062bb7bc04a35165bce3fda765
szogunn <mateusz1_boczek1@interia.pl>
2023-05-23 19:34:46 +0200

    [change] ResponseeEntity moved to BookController from BookService

--- src/main/java/pl/orange/NextDoorBook/book/BookController.java
+++ src/main/java/pl/orange/NextDoorBook/book/BookController.java
@@ -15,13 +15,20 @@ public class BookController {
 
     //TODO trzeba numer id zmienić tak aby był przekazywany przez Spring Security jako ID zalogowanego użytkownika
     @PostMapping(path = "{id}")
-    public ResponseEntity<Book> addBook(@RequestBody Book book, @PathVariable Long id) {
-        return bookService.addBook(book, id);
+    public ResponseEntity<Book> addBook
+    (@RequestBody Book book, @PathVariable Long id) {
+        bookService.addBook(book, id);
+        return ResponseEntity
+                .status(200)
+                .build();
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Book> deleteBook(@PathVariable Long id) {
-        return bookService.deleteBook(id);
+        bookService.deleteBook(id);
+        return ResponseEntity
+                .status(200)
+                .build();
     }
     @GetMapping(path = "/{bookGenre}")
     public ResponseEntity<List<Book>> getBooksByGenre(@PathVariable BookGenre bookGenre){
