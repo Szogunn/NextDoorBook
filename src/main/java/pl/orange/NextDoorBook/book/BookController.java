09b407239e042e9de321a6926ee98d03537e1210
szogunn <mateusz1_boczek1@interia.pl>
2023-05-25 17:50:52 +0200

    [add] implemented PatchMapping [change] changed returned type from Controllers methods

--- src/main/java/pl/orange/NextDoorBook/book/BookController.java
+++ src/main/java/pl/orange/NextDoorBook/book/BookController.java
@@ -1,10 +1,12 @@
 package pl.orange.NextDoorBook.book;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,15 +14,15 @@ import java.util.List;
 public class BookController {
 
     private final BookService bookService;
+    private final ObjectMapper objectMapper;
+
 
     //TODO trzeba numer id zmienić tak aby był przekazywany przez Spring Security jako ID zalogowanego użytkownika
     @PostMapping(path = "{id}")
-    public ResponseEntity<Book> addBook
-    (@RequestBody Book book, @PathVariable Long id) {
-        bookService.addBook(book, id);
+    public ResponseEntity<Book> addBook(@RequestBody Book book, @PathVariable Long id) {
         return ResponseEntity
                 .status(200)
-                .build();
+                .body(bookService.addBook(book, id));
     }
 
     @DeleteMapping(path = "/{id}")
@@ -32,10 +34,18 @@ public class BookController {
     }
     @GetMapping(path = "/{bookGenre}")
     public ResponseEntity<List<Book>> getBooksByGenre(@PathVariable BookGenre bookGenre) {
-        List<Book> booksByGenre = bookService.getBooksByGenre(bookGenre);
         return ResponseEntity
                 .status(200)
-                .body(booksByGenre);
+                .body(bookService.getBooksByGenre(bookGenre));
     }
-}
+
+    @PatchMapping(path = "{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Map<Object,Object> fields){
+
+        return ResponseEntity
+                .status(200)
+                .body(bookService.updateBook(id, fields));
+    }
+
+    }
 
