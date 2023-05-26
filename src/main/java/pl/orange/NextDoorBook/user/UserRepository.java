77e1a37c0d5b51601c68d190fc53cb873b4f22c2
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-26 21:48:55 +0200

    [refactor] UserRepository name correct, add save method

--- src/main/java/pl/orange/NextDoorBook/user/UserRepository.java
+++ src/main/java/pl/orange/NextDoorBook/user/UserRepository.java
@@ -3,21 +3,21 @@ package pl.orange.NextDoorBook.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import pl.orange.NextDoorBook.address.Address;
-import pl.orange.NextDoorBook.comment.Comment;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 @RequiredArgsConstructor
-public class UserRepostiory {
+public class UserRepository {
     private final IUserRepository iUserRepository;
-
     public void addUser(User user){
         iUserRepository.save(user);
     }
     public void deleteUserById(Long id){
         iUserRepository.deleteById(id);
+    }
+    public User save(User user){
+        return iUserRepository.save(user);
     }
     public Optional<User> getUserById(Long id){
         return iUserRepository.findById(id);
