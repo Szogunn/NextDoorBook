65073f75dac4e1482b899eee505da13325c4b10c
szogunn <mateusz1_boczek1@interia.pl>
2023-05-28 21:14:45 +0200

    [change] changed returned type from Optional List to simple List [add] added method getAllBooks

--- src/main/java/pl/orange/NextDoorBook/book/BookRepository.java
+++ src/main/java/pl/orange/NextDoorBook/book/BookRepository.java
@@ -1,18 +1,18 @@
 package pl.orange.NextDoorBook.book;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Repository;
 import pl.orange.NextDoorBook.author.Author;
 import pl.orange.NextDoorBook.author.AuthorRepository;
 import pl.orange.NextDoorBook.user.User;
 import pl.orange.NextDoorBook.user.UserRepostiory;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class BookRepository {
@@ -21,18 +21,11 @@ public class BookRepository {
     private final UserRepostiory userRepostiory;
     private final AuthorRepository authorRepository;
 
+
     public Book addBook(Book book, Long id) {
         User userById = userRepostiory.getUserById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + id));
         book.setOwner(userById);
-
-//        //TODO continue implementation for authors
-//        List<Author> authors = new ArrayList<>();
-//        //pobieram listę autorów z JSONA
-//        for (Author author : new ArrayList<>(book.getAuthors())) {
-//            //Interuję po niej i szukam czy jest w bazie danych, jeśli jest to dodaję do listy authorów
-//            authors.add(authorRepository.getAuthor(author).orElseThrow());
-//        }
 
         return iBookRepository.save(book);
     }
@@ -44,19 +37,18 @@ public class BookRepository {
     public Optional<Book> getBookByID(Long id) {
         return iBookRepository.findById(id);
     }
+    public List<Book> getAllBooks() {
+        return iBookRepository.findAll();
+    }
+
 
     public Book updateBook(Book book) {
         return iBookRepository.save(book);
-
-
-//        iBookRepository.updateBook(id, book.getTittle(), book.getIsbn(),
-//                book.getNumPages(), book.getLanguage(), book.getPublisher(),
-//                book.getPublishedYear(), book.getBookGenre(),
-//                book.getAuthors(), book.getOwner());
     }
 
-    public Optional<List<Book>> getBooksByGenre(BookGenre bookGenre) {
-        return iBookRepository.findBooksByBookGenre(bookGenre);
+
+    public List<Book> getBooksByGenre(BookGenre bookGenre) {
+        return iBookRepository.findByBookGenre(bookGenre);
     }
 
 
