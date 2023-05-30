e471eb0bb0aa63084bf23b3879c585cdf0fd7423
Bartek <bartlomiejtolak7@gmail.com>
2023-05-30 19:12:38 +0200

    [add]RateIllegalArgumentException

---
+++ src/main/java/pl/orange/NextDoorBook/comment/exceptions/RateIllegalArgumentException.java
@@ -0,0 +1,11 @@
+package pl.orange.NextDoorBook.comment.exceptions;
+
+
+public class RateIllegalArgumentException extends RuntimeException {
+
+    public RateIllegalArgumentException(String message) {
+        super(message);
+    }
+
+
+}
