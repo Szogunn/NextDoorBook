c2f5d6c75067ef34834d319e89e141a26b75f610
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-30 21:22:11 +0200

    [add] add methods addUser and getUserById

--- src/main/java/pl/orange/NextDoorBook/user/UserController.java
+++ src/main/java/pl/orange/NextDoorBook/user/UserController.java
@@ -1,21 +1,30 @@
 package pl.orange.NextDoorBook.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.orange.NextDoorBook.user.dto.UserDTO;
 
 @RestController
 @RequestMapping(path = "/api/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepostiory userRepostiory;
+    private final UserService userService;
 
     @PostMapping(path = "")
-    public void addUser(@RequestBody User user) {
-        userRepostiory.addUser(user);
+    public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO userToAdd) {
+        return ResponseEntity
+                .status(200)
+                .body(userService.addUser(userToAdd));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity
+                .status(200)
+                .body(userService.getUserById(id));
+    }
+
 
 }
