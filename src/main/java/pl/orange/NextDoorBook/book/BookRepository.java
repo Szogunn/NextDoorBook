f354be7f922d9ae6b94ee73122883da33927760f
szogunn <mateusz1_boczek1@interia.pl>
2023-05-30 20:54:25 +0200

    [add] implemented logs for every method in class

--- src/main/java/pl/orange/NextDoorBook/book/BookRepository.java
+++ src/main/java/pl/orange/NextDoorBook/book/BookRepository.java
@@ -1,16 +1,13 @@
 package pl.orange.NextDoorBook.book;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Repository;
-import pl.orange.NextDoorBook.author.Author;
-import pl.orange.NextDoorBook.author.AuthorRepository;
 import pl.orange.NextDoorBook.user.User;
 import pl.orange.NextDoorBook.user.UserRepostiory;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Repository
@@ -19,35 +16,50 @@ public class BookRepository {
 
     private final IBookRepository iBookRepository;
     private final UserRepostiory userRepostiory;
-    private final AuthorRepository authorRepository;
 
 
     public Book addBook(Book book, Long id) {
+        log.info("[HIBERNATE] Checking if user exist in database");
         User userById = userRepostiory.getUserById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + id));
         book.setOwner(userById);
 
-        return iBookRepository.save(book);
+        log.info("[HIBERNATE] Saving book to database from addBook method");
+        Book result = iBookRepository.save(book);
+        log.info("Book authors to save: " + book.getAuthors());
+        return result;
     }
 
     public void deleteBookByID(Long id) {
+        log.info("[HIBERNATE] deleting from database book with id: " +id);
         iBookRepository.deleteById(id);
     }
 
     public Optional<Book> getBookByID(Long id) {
+        log.info("[HIBERNATE] looking in database for book with id: " + id);
         return iBookRepository.findById(id);
     }
+
     public List<Book> getAllBooks() {
-        return iBookRepository.findAll();
+        log.info("[HIBERNATE] looking in database for all books existing in db");
+        List<Book> all = iBookRepository.findAll();
+
+        for (Book book : all) {
+            log.info("Authors from findAll method: " + book.getAuthors());
+        }
+
+        return all;
     }
 
 
     public Book updateBook(Book book) {
+        log.info("[HIBERNATE] saving book: " + book  + " into database");
         return iBookRepository.save(book);
     }
 
 
     public List<Book> getBooksByGenre(BookGenre bookGenre) {
+        log.info("[HIBERNATE] looking in database for all books with genre: " + bookGenre);
         return iBookRepository.findByBookGenre(bookGenre);
     }
 
