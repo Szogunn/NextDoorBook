2a6405d03272fdcae8fd5aab62b398c533c6b584
Bartek <bartlomiejtolak7@gmail.com>
2023-05-30 21:05:48 +0200

    [add] add class ExchangeDTOMapper

---
+++ src/main/java/pl/orange/NextDoorBook/exchange/dto/ExchangeDTOMapper.java
@@ -0,0 +1,32 @@
+package pl.orange.NextDoorBook.exchange.dto;
+
+import org.springframework.stereotype.Service;
+import pl.orange.NextDoorBook.exchange.Exchange;
+
+
+@Service
+
+public class ExchangeDTOMapper {
+
+    public ExchangeDTO apply(Exchange exchange) {
+        return new ExchangeDTO(
+                exchange.getId(),
+                exchange.getStartRent(),
+                exchange.getEndRent(),
+                exchange.getOwner(),
+                exchange.getRenter(),
+                exchange.getBook()
+        );
+    }
+
+    public Exchange apply(ExchangeDTO exchangeDTO) {
+        return new Exchange(
+                exchangeDTO.id(),
+                exchangeDTO.startRent(),
+                exchangeDTO.endRent(),
+                exchangeDTO.owner(),
+                exchangeDTO.renter(),
+                exchangeDTO.book()
+        );
+    }
+}
