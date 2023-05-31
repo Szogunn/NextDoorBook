dddae94a9821fd43032f02510d9a523cc9062068
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-31 17:53:33 +0200

    [add] rename UserAddressDTO to AddressAddDTO

--- src/main/java/pl/orange/NextDoorBook/address/DTO/UserAddressDTO.java
+++
@@ -1,9 +0,0 @@
-package pl.orange.NextDoorBook.address.DTO;
-
-public record UserAddressDTO (String cityName,
-                             String street,
-                             int numberHouse,
-                             int zipCode,
-                             String district) {
-
-}
