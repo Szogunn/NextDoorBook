20899f4f598096a4fff6dc6f431636207a7b3b6b
Bartek <bartlomiejtolak7@gmail.com>
2023-05-31 19:23:26 +0200

    [add]add save method

--- src/main/java/pl/orange/NextDoorBook/exchange/ExchangeController.java
+++ src/main/java/pl/orange/NextDoorBook/exchange/ExchangeController.java
@@ -1,32 +1,46 @@
 package pl.orange.NextDoorBook.exchange;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import pl.orange.NextDoorBook.exchange.dto.ExchangeDTO;
+import pl.orange.NextDoorBook.exchange.dto.ExchangeDTOMapper;
 
 @Controller
 @RequiredArgsConstructor
 public class ExchangeController {
     private final ExchangeService exchangeService;
+    private final ExchangeDTOMapper exchangeDTOMapper;
 
     @PostMapping("/exchanges")
-    public ResponseEntity<?> addExchange(Exchange exchange){
-        return exchangeService.addExchange(exchange);
+    public ResponseEntity<ExchangeDTO> addExchange(@RequestBody ExchangeDTO exchangeDTO){
+        exchangeService.addExchange(exchangeDTOMapper.mapToEntity(exchangeDTO));
+        return ResponseEntity
+                .status(200)
+                .body(exchangeDTO);
     }
 
     @DeleteMapping("/exchanges/{id}")
-    public ResponseEntity<?> deleteExchangeById(@PathVariable Long id){
-        return exchangeService.deleteExchangeById(id);
+    public ResponseEntity<ExchangeDTO> deleteExchangeById(@PathVariable Long id){
+        exchangeService.deleteExchangeById(id);
+        return ResponseEntity
+                .status(200)
+                .build();
     }
 
     @GetMapping("/exchanges/{id}")
-    public ResponseEntity<?> getExchangeById(@PathVariable Long id){
-        return exchangeService.getExchangeById(id);
+    public ResponseEntity<ExchangeDTO> getExchangeById(@PathVariable Long id){
+        return ResponseEntity
+                .status(200)
+                .body(exchangeDTOMapper.mapToDTO(exchangeService.getExchangeById(id)));
     }
 
-    @PutMapping("/echanges/{id}")
-    public ResponseEntity<?> updateExchange(@PathVariable Long id,@RequestBody Exchange toUpdate){
-        return exchangeService.updateExchange(id,toUpdate);
+    @PutMapping("/echanges")
+    public ResponseEntity<ExchangeDTO> updateExchange(@RequestBody ExchangeDTO exchangeDTO){
+        return ResponseEntity
+                .status(200)
+                .body(exchangeDTOMapper.mapToDTO(exchangeService.updateExchange(exchangeDTOMapper.mapToEntity(exchangeDTO))));
     }
 }
