fe9923fd841a82ff190242a07d01ddf8e6c48604
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-02 10:37:02 +0200

    [add] create AuthorAddDTO and add method to map in AuthorDTOMapper

---
+++ src/main/java/pl/orange/NextDoorBook/author/dto/AuthorAddDTO.java
@@ -0,0 +1,11 @@
+package pl.orange.NextDoorBook.author.dto;
+
+public record AuthorAddDTO(
+        Long id,
+        String firstName,
+        String lastName,
+        String nationality
+) {
+
+
+}
