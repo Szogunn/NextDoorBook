be7edf92aba30e1d43e7fa74d481a18dfc87f985
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-02 11:49:35 +0200

    [add] in AuthorRepository update Author method use default save and return Author, remove unused Modifying query

--- src/main/java/pl/orange/NextDoorBook/author/IAuthorRepository.java
+++ src/main/java/pl/orange/NextDoorBook/author/IAuthorRepository.java
@@ -2,28 +2,13 @@ package pl.orange.NextDoorBook.author;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import pl.orange.NextDoorBook.comment.Comment;
-import pl.orange.NextDoorBook.user.User;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface IAuthorRepository extends JpaRepository<Author, Long> {
-
-    @Query(value = "UPDATE Author SET firstName = :firstName" +
-            ",lastName =:lastName" +
-            ",nationality=:nationality" +
-            " WHERE id =:id")
-    @Modifying
-    @Transactional
-    void updateAuthor(@Param("id") Long id,
-                      @Param("firstName") String firstName,
-                      @Param("lastName") String lastName,
-                      @Param("nationality") String nationality);
 
 
     @Query("SELECT a FROM Author a " +
