97985bd057753c7472b19e4b913a283c40ab1956
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-03 14:02:08 +0200

    [add] create ExchangeOwnerException, handling exception in ExchangeGlobalExceptionHandler

---
+++ src/main/java/pl/orange/NextDoorBook/exchange/exception/ExchangeOwnerException.java
@@ -0,0 +1,5 @@
+package pl.orange.NextDoorBook.exchange.exception;
+
+public class ExchangeOwnerException extends RuntimeException{
+    public ExchangeOwnerException(String message){super(message);}
+}
